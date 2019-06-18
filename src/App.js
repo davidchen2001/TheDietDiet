0aceafaf521bd69498ba81d2f333e109e884c668
Zronen <ZenronXsword@live.com>
2019-06-18 15:00:54 -0400

    Added a popup and sample text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Popup from 'reactjs-popup'
+
+
+const Greetings = (props) => <div> Howdy! {props.first} {props.last}!</div>;
 
 function App() {
   return (
     <div className="App">
+
+const PopupExample =  () => (
+  <Popup trigger={<button> Trigger</button>} position="right center">
+    <div><Greetings first="John" last="Smith" /></div>
+  </Popup>
+)
       <header className="App-header">
+        
+        <Greetings first="John" last="Smith" />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -21,6 +34,9 @@ function App() {
       </header>
     </div>
   );
+
 }
+
+
 
 export default App;
