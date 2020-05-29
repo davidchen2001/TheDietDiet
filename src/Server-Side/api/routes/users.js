73a1a7d0dbb64482de8a13294c952731f27cc196
Dioceus <chuwei.chen2001@gmail.com>
2020-05-29 13:36:19 -0400

    Users API refactored

--- src/Server-Side/api/routes/users.js
+++ src/Server-Side/api/routes/users.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
- 
+const auth = require('../../Database/middleware/auth')
 const User = require('../../Database/models/UserModel');
 
 /**
- * @route   POST /registration
+ * @route   POST /auth/register
  * @desc    Register user
  * @access  Public
  */
 
 
-router.post('/', (req, res) => {
+router.post('/register', (req, res) => {
   const { name, username, emailAddress, password, isHelper } = req.body;
 
   // Simple validation
@@ -61,5 +61,63 @@ router.post('/', (req, res) => {
 
     })
 });
+
+/**
+ * @route   POST /auth/login
+ * @desc    Login user
+ * @access  Public
+ */
+
+router.post('/login', (req, res) => {
+  
+    const { emailAddress, password} = req.body;
+  
+    // Simple validation
+     if( (String(emailAddress) === 0 || String(password) === 0) ) {
+      return res.status(400).json({ msg: 'Please enter all fields' });
+     }
+  
+     User.findOne({ emailAddress })
+     .then(user => {
+         if(!user) return res.status(400).json({ msg: 'User does not exist' });
+  
+        //Validating password
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if(!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+  
+          jwt.sign(
+            { id: user.id },
+            config.get('jwtSecret'),
+            { expiresIn: 3600 },
+            (err, token) => {
+              if(err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  username: user.username,
+                  emailAddress: user.emailAddress
+                }
+              });
+            }
+          )
+        })
+        
+      })
+  });
+
+/**
+ * @route   POST /auth/user
+ * @desc    Get user data 
+ * @access  Private 
+ */
+
+router.get('/user', auth, (req, res) => {
+    User.findById(req.user.id)
+      .select('-password')
+      .then(user => res.json(user));
+  });
 
 module.exports = router;
