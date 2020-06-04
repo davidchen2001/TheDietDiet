d6bebc1c41c95038547c80d2460bd456e4ac1c62
Dioceus <chuwei.chen2001@gmail.com>
2020-06-04 16:23:24 -0400

    API routes can be accessed with any URL

--- src/Actions/ImageAction.js
+++ src/Actions/ImageAction.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 import {IMAGE_UPLOADED, IMAGE_UPLOAD_ERROR } from './types';
 import {returnError} from './ErrorAction'; 
 
-export const uploadImage = ({ imageName}) => dispatch => {
+export const uploadImage = ({ imageName }) => dispatch => {
 
     const body = JSON.stringify({ imageName} )
 
     axios
-    .post('http://localhost:5000/images/upload', body)
+    .post('/images/upload', body)
     .then(res =>
         dispatch({
             type: IMAGE_UPLOADED,
@@ -23,3 +23,4 @@ export const uploadImage = ({ imageName}) => dispatch => {
 
     })
 }
+
