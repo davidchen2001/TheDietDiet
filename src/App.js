f35f893e948af4c28853ff938bd658abfe9251eb
Dioceus <chuwei.chen2001@gmail.com>
2020-08-26 12:54:12 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, {Component} from 'react';
 import './App.css';
 import Nut from '../src/assets/DietDietLogo.png'
 
-import { BrowserRouter as Router, Route, Link, NavLink } from  'react-router-dom'
+import { BrowserRouter as Router, Route, Link, NavLink, Switch } from  'react-router-dom'
 import HomePage from './pages/HomePage/HomePage';
-import SignInForm from './pages/SignInForm';
-import SignUpForm from './pages/SignUpForm';
+import SignInForm from './pages/AuthPage/SignInForm';
+import SignUpForm from './pages/AuthPage/SignUpForm';
 import CafesPage from './pages/CafesPage';
 import UploadPage from './pages/UploadPage';
 import ProfilePage from './pages/ProfilePage';
@@ -14,6 +14,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import { loadUser } from './actions/AuthAction';
+
 class App extends Component{
   componentDidMount() {
     store.dispatch(loadUser());
@@ -23,8 +24,8 @@ class App extends Component{
     <Router>
     {this.props.children}
       <Provider store={store}>
-      <div className="App">
-        
+
+          <Switch>
           <Route exact path="/" component={HomePage}>
             </Route>
 
@@ -44,8 +45,7 @@ class App extends Component{
           <Route exact path ="/profile" component={ProfilePage}>
           </Route>
 
-          
-        </div>
+          </Switch>
         </Provider>
     </Router>
   );
@@ -53,43 +53,3 @@ class App extends Component{
 }
 
 export default App;
-
-
-
-  /*
-  const PopupExample =  () => (
-    <Popup trigger={<button> Trigger</button>} position="right center">
-      <div><Greetings first="John" last="Smith" /></div>
-    </Popup>
-  )
-        <header className="App-header">
-          
-           
-          <Greetings first="John" last="Smith" />
-          
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            
-          >
-            Learn React
-          </a>
-        </header>
-
-
-          <div className="FormCenter">
-          <form className="FormFields" onSubmit={this.handleSubmit}>
-            <div className="FormField">
-              <label className="FormField__Label"htmlfor="name">Full Name</label>
-              <input type="text" id="name" className="FormField__Input" placeholder="Enter full name here" name="name"/>
-              </div>
-            </form>
-          </div>
-
-        */
